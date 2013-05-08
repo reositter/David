1c38e9f49e06e4d58b89920fd97fd3b1562faca3
davidxcheng <davidxcheng@gmail.com>
2013-05-08 08:30:15 +0200

    Added view for test calls to the pim api

---
+++ PimIntegration/PimIntegration.Host/Modules/DevToolsModule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Nancy;
+using PimIntegration.Tasks;
+using PimIntegration.Tasks.PimApi;
+using StructureMap;
+
+namespace PimIntegration.Host.Modules
+{
+	public class DevToolsModule : Nancy.NancyModule
+	{
+		public DevToolsModule()
+		{
+			Get["/devtools"] = o =>
+			{
+				o.Title = "Dev Tools";
+				return View["devtools.cshtml", o];
+			};
+
+			Post["/products"] = o =>
+			{
+				Log.ForCurrent.Info("POST /products");
+
+				var pimQueryService = ObjectFactory.Container.GetInstance<IPimQueryService>();
+
+				var products = pimQueryService.GetNewProductsSince(DateTime.Now.AddHours(-4));
+
+
+				var response = (Response) products.Length.ToString();
+				response.ContentType = "applicatin/json";
+				return response;
+			};
+		}
+	}
+}
